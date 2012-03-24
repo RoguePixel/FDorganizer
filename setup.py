b423580f0c1ad8ba43eba9dd0835f6c106ab4262
Derek <sjukledighet@gmail.com>
2012-03-24 03:19:51 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 wget http://code.google.com/p/mutagen/downloads/detail?name=mutagen-1.20.tar.gz&can=2&q=
+#find way to download argparse module on all OS'
 
 #python /path/to/setup.py
